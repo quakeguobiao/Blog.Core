3ff9a2cc4630e2819bfcbf8bbd3c1dfce31f3c05
quakeguobiao <quakeguobiao@gmail.com>
2022-04-22 15:58:39 +0800

    修改dockerpublish文件.

--- Blog.Core.Publish.Docker.sh
+++ Blog.Core.Publish.Docker.sh
@@ -1,18 +1,18 @@
 ﻿# 停止容器
-docker stop apkcontainer
+docker stop blcoreapi
 # 删除容器
-docker rm apkcontainer
+docker rm blcoreapi
 # 删除镜像
-docker rmi laozhangisphi/apkimg
+docker rmi quakeguobiao/blogcoreapi
 # 切换目录
-cd /home/Blog.Core
+cd /home/blogcore/Blog.Core
 # 发布项目
 ./Blog.Core.Publish.Linux.sh
 # 进入目录
-cd /home/Blog.Core/.PublishFiles
+cd /home/blogcore/Blog.Core/.PublishFiles
 # 编译镜像
-docker build -t laozhangisphi/apkimg .
+docker build -t quakeguobiao/blogcoreapi
 # 生成容器
-docker run --name=apkcontainer -d -v /etc/localtime:/etc/localtime -it -p 9291:9291 laozhangisphi/apkimg
+docker run --name=blcoreapi -d -v /etc/localtime:/etc/localtime -it -p 9291:9291 quakeguobiao/blogcoreapi
 # 启动容器
-docker start apkcontainer
+docker start blcoreapi
