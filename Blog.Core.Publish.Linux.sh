ce00e76b94a49ad0c59202052eabc1b5e10d77a2
quakeguobiao <quakeguobiao@gmail.com>
2022-04-23 21:17:12 +0800

    修改了linuxpublish文件路径

--- Blog.Core.Publish.Linux.sh
+++ Blog.Core.Publish.Linux.sh
@@ -1,8 +1,8 @@
 git pull;
 find .PublishFiles/ -type f -and ! -path '*/wwwroot/images/*' ! -name 'appsettings.*' |xargs rm -rf
 dotnet build;
-rm -rf /home/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles;
-dotnet publish -o /home/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles;
+rm -rf /home/blogcore/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles;
+dotnet publish -o /home/blogcore/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles;
 # cp -r /home/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles ./;
-awk 'BEGIN { cmd="cp -ri /home/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles ./"; print "n" |cmd; }'
+awk 'BEGIN { cmd="cp -ri /home/blogcore/Blog.Core/Blog.Core.Api/bin/Debug/.PublishFiles ./"; print "n" |cmd; }'
 echo "Successfully!!!! ^ please see the file .PublishFiles";
