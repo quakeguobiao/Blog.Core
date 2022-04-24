a7d2ae97305fe9d4e2b22713c089bf03965139e3
quakeguobiao <quakeguobiao@gmail.com>
2022-04-24 09:05:35 +0800

    继续修改路径

--- Blog.Core.Publish.Docker.sh
+++ Blog.Core.Publish.Docker.sh
@@ -1,9 +1,9 @@
 ﻿# 停止容器
-docker stop blcoreapi
+#docker stop blcoreapi
 # 删除容器
-docker rm blcoreapi
+#docker rm blcoreapi
 # 删除镜像
-docker rmi quakeguobiao/blogcoreapi
+#docker rmi quakeguobiao/blogcoreapi
 # 切换目录
 cd /home/blogcore/Blog.Core
 # 发布项目
@@ -12,7 +12,8 @@ cd /home/blogcore/Blog.Core
 cd /home/blogcore/Blog.Core/.PublishFiles
 # 编译镜像
 docker build -t quakeguobiao/blogcoreapi
+
 # 生成容器
-docker run --name=blcoreapi -d -v /etc/localtime:/etc/localtime -it -p 9291:9291 quakeguobiao/blogcoreapi
+#docker run --name=blcoreapi -d -v /etc/localtime:/etc/localtime -it -p 9291:9291 quakeguobiao/blogcoreapi
 # 启动容器
-docker start blcoreapi
+#docker start blcoreapi
