d94d5dcf7fbcf32a49a6550ce16d7e8916047402
quakeguobiao <quakeguobiao@gmail.com>
2022-04-24 09:27:26 +0800

    少了个点

--- Blog.Core.Publish.Docker.sh
+++ Blog.Core.Publish.Docker.sh
@@ -11,7 +11,7 @@ cd /home/blogcore/Blog.Core
 # 进入目录
 cd /home/blogcore/Blog.Core/.PublishFiles
 # 编译镜像
-docker build -t quakeguobiao/blogcoreapi
+docker build -t quakeguobiao/blogcoreapi .
 
 # 生成容器
 #docker run --name=blcoreapi -d -v /etc/localtime:/etc/localtime -it -p 9291:9291 quakeguobiao/blogcoreapi
