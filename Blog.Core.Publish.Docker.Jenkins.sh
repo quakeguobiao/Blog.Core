31c9c4a191fb8df82bd998b7588b3555a3dc17ff
quakeguobiao <quakeguobiao@gmail.com>
2022-05-07 20:05:18 +0800

    还是jenkins修改.

--- Blog.Core.Publish.Docker.Jenkins.sh
+++ Blog.Core.Publish.Docker.Jenkins.sh
@@ -6,15 +6,15 @@ dotnet publish
 echo "Successfully!!!! ^ please see the file ."
 cd bin/Debug/net6.0/publish/
 
-rm -f appsettings.json
-cp -rf /var/jenkins_home/workspace/SecurityConfig/Blog.Core/appsettings.json appsettings.json
+#rm -f appsettings.json
+#cp -rf /var/jenkins_home/workspace/SecurityConfig/Blog.Core/appsettings.json appsettings.json
 
 #docker stop apkcontainer
 #docker rm apkcontainer
 #docker rmi laozhangisphi/apkimg
 
 chmod 777 StopContainerImg.sh
-./StopContainerImg.sh apkcontainer laozhangisphi/apkimg
+./StopContainerImg.sh apkcontainer quakeguobiao/blogcoreapi
 
-docker build -t laozhangisphi/apkimg .
-docker run --name=apkcontainer -d -v /data/blogcore/appsettings.json:/app/appsettings.json -v /data/blogcore/Log/:/app/Log -v /etc/localtime:/etc/localtime -it -p 9291:9291 laozhangisphi/apkimg
+docker build -t quakeguobiao/blogcoreapi .
+docker run --name=apkcontainer -d -v /home/blogcore/appsettings.json:/app/appsettings.json -v /home/blogcore/Log/:/app/Log -v /etc/localtime:/etc/localtime -it -p 9291:9291 quakeguobiao/blogcoreapi
